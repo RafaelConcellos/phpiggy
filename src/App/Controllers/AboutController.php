2d8b33372c82f37ded9b6b1840b0f286b87c5f71
RafaelConcellos <136651016+RafaelConcellos@users.noreply.github.com>
2024-11-30 09:46:22 -0300

    Saved before 150.Autoloading Functions

---
+++ src/App/Controllers/AboutController.php
@@ -0,0 +1,28 @@
+<?php
+#AboutController
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
+class AboutController
+{
+
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
+
+    public function about()
+    {
+        echo $this->view->render('about.php', [
+            'title' => 'About',
+            'dangerousData' => '<script>alert(123)</script>',
+        ]);
+    }
+}
